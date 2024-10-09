2c659a5278776c530edd54d264cb22c24d164c28
Luca Tierno <lucatiernoboca@gmail.com>
2024-10-09 20:15:08 -0300

    ADD: variables bd

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "dotenv/config";
 import expres from "express";
 import cros from "cors";
 import { router } from "./routes";
+import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3001;
 
 const app = expres();
 app.use(cros());
-app.use(router)
+app.use(router);
+db().then(() => console.log("Conectado a la BD"));
 
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto ${PORT}`);
