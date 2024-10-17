a3ed0dec427bc3b05c296d731c08294e35490308
Joaquin <joaqobianchi13@gmail.com>
2024-10-17 17:53:54 -0300

    Actualizando nombre de las rutas y la funcion que las crea

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,29 @@
 import { Router } from "express";
 import { readdirSync } from "fs";
+import path from "path";
 
 const PATH_ROUTER = `${__dirname}`;
 
 const router = Router();
 
+// Función actualizada para limpiar el nombre de archivo y obtener solo el nombre antes de ".routes.ts"
 const cleanFileName = (fileName: string) => {
-  const file = fileName.split(".").shift();
-  console.log(file);
+  const file = fileName.split(".").slice(0, -1).join("."); // Remueve la última extensión (ejemplo: ".ts")
 
-  return file;
+  // Si el archivo sigue el patrón "nombre-ruta.routes.ts", obtiene el nombre antes de ".routes"
+  if (file.endsWith(".routes")) {
+    return file.split(".routes").shift(); // Obtiene solo el nombre base (antes de ".routes")
+  }
+
+  return null; // Si el archivo no sigue el patrón, devuelve null
 };
 
+// Carga de rutas
 readdirSync(PATH_ROUTER).filter((fileName) => {
   const cleanName = cleanFileName(fileName);
-  if (cleanName !== "index") {
-    import(`./${cleanName}`).then((moduleRouter) => {
+  if (cleanName && cleanName !== "index") {
+    import(path.join(PATH_ROUTER, fileName)).then((moduleRouter) => {
       console.log(`Cargando la ruta... /${cleanName}`);
-
       router.use(`/${cleanName}`, moduleRouter.router);
     });
   }
--- src/schemas/exercise.schema.ts
+++ src/schemas/exercise.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const CreateExerciseSchema = z.object({
   body: z.object({
-    name: z.string(),
-    image: z.string(),
+    name: z.string().min(3).max(20),
+    image: z.string().optional(),
     muscleGroups: z.string().array(),
   }),
 });
