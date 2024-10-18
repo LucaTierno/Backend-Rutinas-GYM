ff75c881b1585c4d1a497529d0ab403c86d78e49
Joaquin <joaqobianchi13@gmail.com>
2024-10-17 21:11:55 -0300

    Validaciones para que el grupo muscular sea el mismo que tiene la bd

--- src/schemas/exercise.schema.ts
+++ src/schemas/exercise.schema.ts
@@ -1,9 +1,12 @@
+import { MuscleGroup } from "@prisma/client";
 import { z } from "zod";
+
+const MuscleGroupEnum = z.nativeEnum(MuscleGroup);
 
 export const CreateExerciseSchema = z.object({
   body: z.object({
     name: z.string().min(3).max(20),
     image: z.string().optional(),
-    muscleGroups: z.string().array(),
+    muscleGroups: z.array(MuscleGroupEnum).min(1),
   }),
 });
