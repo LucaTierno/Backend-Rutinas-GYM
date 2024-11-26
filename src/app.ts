6f77f0114c091214bec5012f7686d52bf84c1d6c
Joaquin <joaqobianchi13@gmail.com>
2024-11-26 14:45:54 -0300

    Remove cors options

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,8 @@ const PORT = process.env.PORT || 3001;
 // const originsConfig = [process.env.FRONTEND_URL || ""];
 
 const app = express();
-app.use(
-  cors()
-);
-app.options("*", cors());
+app.use(cors());
+// app.options("*", cors());
 app.use(express.json());
 app.use(router);
 
