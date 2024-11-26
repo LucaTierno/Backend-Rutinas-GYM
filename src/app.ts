ea0a491a28514b0f81cb50ea9454f6461539ae82
Joaquin <joaqobianchi13@gmail.com>
2024-11-26 14:42:19 -0300

    Remove cors to localhost

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,11 @@ import { router } from "./routes";
 
 const PORT = process.env.PORT || 3001;
 
-const originsConfig = [process.env.FRONTEND_URL || ""];
+// const originsConfig = [process.env.FRONTEND_URL || ""];
 
 const app = express();
 app.use(
-  cors({
-    origin: originsConfig,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
+  cors()
 );
 app.options("*", cors());
 app.use(express.json());
@@ -22,3 +18,9 @@ app.use(router);
 app.listen(PORT, () => {
   console.log(`Escuchando en el puerto ${PORT}`);
 });
+
+// {
+//   origin: originsConfig,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }
